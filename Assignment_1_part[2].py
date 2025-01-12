7d0613e2c06356e3fcfcee628b8739220b5b1952
Pulkit Garg <pulkitgarg31official@gmail.com>
2025-01-12 20:26:30 +0530

    Initial commit

---
+++ Assignment_1_part[2].py
@@ -0,0 +1,16 @@
+import sys
+num1 = float(sys.argv[1])
+num2 = float(sys.argv[2])
+
+result = num1 + num2
+print(f"The sum of {num1} and {num2} is {result}")
+
+
+str1 = sys.argv[3]
+
+print('Length of string is: ',len(str1))
+
+#Output
+# ###################################>python Assignment_1_part[2].py 10 20 hihello
+# The sum of 10.0 and 20.0 is 30.0
+# Length of string is:  7
